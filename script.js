8e122c8e5d01e343536718be659c41d5f0483b8b
Kilian Albar <kilian.albar@gmail.com>
2021-07-09 19:50:18 +0200

    Modal images + update menu nav

--- script.js
+++ script.js
@@ -30,3 +30,31 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Modal image
+
+let modal = document.getElementById("myModal");
+let images = document.getElementsByClassName("img");
+let modalImg = document.getElementById("img1");
+let captionText = document.getElementById("modalCaption");
+
+// Loop for multiple images 
+
+for (let i = 0; i < images.length; i++) {
+  let img = images[i];
+  img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  }
+}
+
+// Get the close span
+
+let span = document.getElementsByClassName("close")[0];
+
+// Close modal on click X
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
